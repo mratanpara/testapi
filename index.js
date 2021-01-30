42559eb831a2bbb250293094df237126f3822a46
mratanpara <mohitratanpara2001@gmail.com>
2021-01-30 22:50:32 +0530

    bug

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const route = require("./route");
 const { json } = require('body-parser');
-mongoose.connect("mongodb+srv://student:mohit8980@cluster0.gans7.mongodb.net/Book?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb+srv://user1:apstndp16@cluster0.vqjzb.mongodb.net/students?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
 ()=>{
         const app = express();
         app.use("/api",route);
         app.use(bodyParser.urlencoded({extended: true}));
         app.use(express.json());
-        app.listen(process.env.PORT,()=>{
+        app.listen(process.env.PORT || 3000,()=>{
             console.log("Server Started on 3000 !!");
         });
     }
--- route.js
+++ route.js
@@ -2,19 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Book = require('./Model/book');
 const router = express.Router();
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({extended:true}));
 router.use(express.json());
-router.get("/books", async(req, res) => {
+
+router.get("/books",async(req,res)=>{
     const book = await Book.find();
     res.send(book);
 });
-// Create
-router.post("/books", async(req, res) => {
-    const book = new Book({
-        name: req.body.name,
-        qty: req.body.qty
+router.patch('/books/:id',async(req,res)=>{
+    try {
+        const book =await Book.findOne({_id:req.params.id});
+        book.name = req.body.name;
+        book.qty = req.body.qty;
+        await book.save();
+        res.send(book);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+});
+router.post("/books",async(req,res)=>{
+    const book =new Book({
+        name:req.body.name,
+        qty:req.body.qty
+
     });
     await book.save();
     res.send("Record Created");
+});
+router.delete('/books/:id', async (req, res) => {
+    try {
+        await Book.deleteOne({ _id: req.params.id });
+        res.send("Record Deleted");
+    } catch (error) {
+        res.status(404).send({error:"Book Not Found"});
+    }
 });
 module.exports = router;
